1052d028af27196000554308d7f839df3da09a56
ZonaBR <71289282+ZonaBRDev@users.noreply.github.com>
2021-06-17 00:35:56 -0300

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,11 @@
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
